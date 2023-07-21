8008d1a2dc656c4b8db55ba71e7fc62234b57e61
akash <akash97kulakkada@gmail.com>
2023-07-21 13:23:19 +0530

    style added

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputFelid from './components/InputFelid';
+import {Todo} from "./model"
+import TodoList from './components/TodoList';
+
+
+
+const  App: React.FC = () => {
+
+  const [todo ,setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e:React.FormEvent) =>{
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos,{ id: Date.now(), todo, isDone: false}]);
+      setTodo("");
+    }
+  }
+
+  //  console.log(todo);
+  //  console.log(todos);
+  return (
+    <div className="App">
+      <span className='heading'>Taskify</span>
+      <InputFelid todo={todo} setTodo ={setTodo} handleAdd ={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+}
+
+export default App;
